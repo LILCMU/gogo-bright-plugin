f5455174b698cfead14aee4d4c8a31ab9dca1cb4
Marutpong <marutpong08@gmail.com>
2019-01-08 15:33:28 +0700

    Change I2C Bus

--- gogo_bright/gogo_bright/generators.js
+++ gogo_bright/gogo_bright/generators.js
@@ -6,7 +6,7 @@ const filterNumber = function(number) {
 
 Blockly.JavaScript['gogo_bright.input_sensor'] = function(block) {
 	return [
-		'DEV_I2C0.GOGO_BRIGHT().readInput(' + block.getFieldValue('sensor') + ')',
+		'DEV_I2C1.GOGO_BRIGHT().readInput(' + block.getFieldValue('SENSOR') + ')',
 		Blockly.JavaScript.ORDER_ATOMIC
 	];
 };
@@ -19,35 +19,35 @@ Blockly.JavaScript['gogo_bright.action_motor'] = function () {
 	var checkboxC = this.getFieldValue('c')
 	var checkboxD = this.getFieldValue('d')
 
-	var code = 'DEV_I2C0.GOGO_BRIGHT().talkToOutput("' + (checkboxA === 'TRUE' ? 'a' : '') +
+	var code = 'DEV_I2C1.GOGO_BRIGHT().talkToOutput("' + (checkboxA === 'TRUE' ? 'a' : '') +
 		(checkboxB === 'TRUE' ? 'b' : '') +
 		(checkboxC === 'TRUE' ? 'c' : '') +
 		(checkboxD === 'TRUE' ? 'd' : '') +
 		'");\n'
-	if (code === 'DEV_I2C0.GOGO_BRIGHT().talkToOutput("");\n') { code = '\n' }
+	if (code === 'DEV_I2C1.GOGO_BRIGHT().talkToOutput("");\n') { code = '\n' }
 	return code;
 }
 
 Blockly.JavaScript['gogo_bright.motor_action_turn'] = function () {
 	var dropdownTurn = this.getFieldValue('turn')
-	var code = 'DEV_I2C0.GOGO_BRIGHT().' + (dropdownTurn == 'on' ? 'turnOutputOn' : 'turnOutputOff') + '();\n'
+	var code = 'DEV_I2C1.GOGO_BRIGHT().' + (dropdownTurn == 'on' ? 'turnOutputOn' : 'turnOutputOff') + '();\n'
 	return code;
 }
 
 Blockly.JavaScript['gogo_bright.motor_action_power'] = function (block) {
 	var valuePower = Blockly.JavaScript.valueToCode(block, 'power', Blockly.JavaScript.ORDER_NONE)
-	var code = 'DEV_I2C0.GOGO_BRIGHT().setOutputPower(' + filterNumber(valuePower) + ');\n'
+	var code = 'DEV_I2C1.GOGO_BRIGHT().setOutputPower(' + filterNumber(valuePower) + ');\n'
 	return code
 }
 
 Blockly.JavaScript['gogo_bright.motor_action_cw'] = function () {
 	var dropdownThisway = this.getFieldValue('clockwise')
-	var code = 'DEV_I2C0.GOGO_BRIGHT().' + (dropdownThisway == 'cw' ? 'turnOutputCW' : 'turnOutputCCW') + '();\n'
+	var code = 'DEV_I2C1.GOGO_BRIGHT().' + (dropdownThisway == 'cw' ? 'turnOutputCW' : 'turnOutputCCW') + '();\n'
 	return code
 }
 
 Blockly.JavaScript['gogo_bright.motor_action_rd'] = function () {
-	var code = 'DEV_I2C0.GOGO_BRIGHT().reverseOutputDirection();\n'
+	var code = 'DEV_I2C1.GOGO_BRIGHT().reverseOutputDirection();\n'
 	return code
 }
 
@@ -59,29 +59,29 @@ Blockly.JavaScript['gogo_bright.action_servo'] = function () {
 	var checkboxC = this.getFieldValue('c')
 	var checkboxD = this.getFieldValue('d')
 
-	var code = 'DEV_I2C0.GOGO_BRIGHT().talkToServo("' + (checkboxA === 'TRUE' ? '1' : '') +
+	var code = 'DEV_I2C1.GOGO_BRIGHT().talkToServo("' + (checkboxA === 'TRUE' ? '1' : '') +
 		(checkboxB === 'TRUE' ? '2' : '') +
 		(checkboxC === 'TRUE' ? '3' : '') +
 		(checkboxD === 'TRUE' ? '4' : '') +
 		'");\n'
-	if (code === 'DEV_I2C0.GOGO_BRIGHT().talkToServo("");\n') { code = '' }
+	if (code === 'DEV_I2C1.GOGO_BRIGHT().talkToServo("");\n') { code = '' }
 	return code;
 }
 
 Blockly.JavaScript['gogo_bright.servo_seth'] = function (block) {
 	var valueHeading = Blockly.JavaScript.valueToCode(block, 'heading', Blockly.JavaScript.ORDER_NONE)
-	var code = `DEV_I2C0.GOGO_BRIGHT().setServoAngle(${filterNumber(valueHeading)});\n`
+	var code = `DEV_I2C1.GOGO_BRIGHT().setServoAngle(${filterNumber(valueHeading)});\n`
 	return code
 }
 
 Blockly.JavaScript['gogo_bright.servo_turn_cw'] = function (block) {
 	var valueHeading = Blockly.JavaScript.valueToCode(block, 'heading', Blockly.JavaScript.ORDER_NONE)
-	var code = `DEV_I2C0.GOGO_BRIGHT().turnServoCW(${filterNumber(valueHeading)});\n`
+	var code = `DEV_I2C1.GOGO_BRIGHT().turnServoCW(${filterNumber(valueHeading)});\n`
 	return code
 }
 
 Blockly.JavaScript['gogo_bright.servo_turn_ccw'] = function (block) {
 	var valueHeading = Blockly.JavaScript.valueToCode(block, 'heading', Blockly.JavaScript.ORDER_NONE)
-	var code = `DEV_I2C0.GOGO_BRIGHT().turnServoCCW(${filterNumber(valueHeading)});\n`
+	var code = `DEV_I2C1.GOGO_BRIGHT().turnServoCCW(${filterNumber(valueHeading)});\n`
 	return code
 }
